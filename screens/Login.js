fe89a3c67c393124717163fe2279bbe1ccceb0da
clarissefaye <clarissefayetan2004@gmail.com>
2025-02-23 14:43:50 -0800

    Add Login page basic features
    Username input, Password input, Confirm login button and Login with faceid button

--- screens/Login.js
+++ screens/Login.js
@@ -1,1 +1,36 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
+return (
+  <View style={styles.container}>
+    <Text style={styles.text}>Username</Text>
+    <TextInput  
+        style={styles.input} 
+        placeholder='Enter username'
+        onChangeText={text => setUsername(text)}    
+        value={username}
+    />
+    <Text style={styles.text}>Password</Text>
+    <TextInput  
+        style={styles.input} 
+        placeholder='Enter password'
+        onChangeText={text => setPassword(text)}    
+        value={password}
+    />
+    <Text style={styles.text}>Confirm Login</Text>
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => navigation.navigate('Home')}>
+      <Text style={styles.buttonText}>Login</Text>
+    </TouchableOpacity>
+    <Text style={styles.text}>Login with FaceID</Text>
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => navigation.navigate('Home')}>
+      <Text style={styles.buttonText}>Login</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+const styles = StyleSheet.create({          // Add this 
+});
