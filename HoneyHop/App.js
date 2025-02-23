4086b41b4b834b0706e831c1e9cabd9b83259390
Jenna Han <jennahan03@gmail.com>
2025-02-23 14:22:10 -0800

    Create login and signup buttons
    Used TouchableOpacity element

--- HoneyHop/App.js
+++ HoneyHop/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to HoneyHop</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity><Text>Login</Text></TouchableOpacity>
+      <TouchableOpacity><Text>Sign Up</Text></TouchableOpacity>
     </View>
   );
 }
