34c80bb66675a2753f8244fd8a87a3988ff5c7b3
clarissefaye <clarissefayetan2004@gmail.com>
2025-02-23 16:00:11 -0800

    Create GlobalStyles.js and implement in Login.js

--- HoneyHop/screens/Login.js
+++ HoneyHop/screens/Login.js
@@ -1,33 +1,34 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import GlobalStyles from '../styles/GlobalStyles';
 
 
 return (
-  <View style={styles.container}>
-    <Text style={styles.text}>Username</Text>
+  <View style = {GlobalStyles.container}>
+    <Text style = {GlobalStyles.text}>Username</Text>
     <TextInput  
-        style={styles.input} 
+        style={GlobalStyles.input} 
         placeholder='Enter username'
         onChangeText={text => setUsername(text)}    
         value={username}
     />
-    <Text style={styles.text}>Password</Text>
+    <Text style={GlobalStyles.text}>Password</Text>
     <TextInput  
-        style={styles.input} 
+        style={GlobalStyles.input} 
         placeholder='Enter password'
         onChangeText={text => setPassword(text)}    
         value={password}
     />
-    <Text style={styles.text}>Confirm Login</Text>
+    <Text style={GlobalStyles.text}>Confirm Login</Text>
     <TouchableOpacity
-      style={styles.button}
+      style={GlobalStyles.button}
       onPress={() => navigation.navigate('Home')}>
-      <Text style={styles.buttonText}>Login</Text>
+      <Text style={GlobalStyles.buttonText}>Login</Text>
     </TouchableOpacity>
-    <Text style={styles.text}>Login with FaceID</Text>
+    <Text style={GlobalStyles.text}>Login with FaceID</Text>
     <TouchableOpacity
-      style={styles.button}
+      style={GlobalStyles.button}
       onPress={() => navigation.navigate('Home')}>
-      <Text style={styles.buttonText}>Login</Text>
+      <Text style={GlobalStyles.buttonText}>Login</Text>
     </TouchableOpacity>
   </View>
 );
---
+++ HoneyHop/styles/GlobalStyles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+const GlobalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    color: "#333",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+  button: {
+    backgroundColor: "green",
+    padding: 9,
+    margin: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+  },
+});
