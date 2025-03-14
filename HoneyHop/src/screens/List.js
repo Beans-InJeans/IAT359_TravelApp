9cecdd34f1dbdb53bfb8fe9576c9f256adce5bc1
Jenna Han <jennahan03@gmail.com>
2025-03-14 15:16:40 -0700

    Add styles to List.js
    Placeholder styles

--- HoneyHop/src/screens/List.js
+++ HoneyHop/src/screens/List.js
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native';
+import GlobalStyles from '../../styles/GlobalStyles';
 
 export default function List() {
   return(
-    <View>
-      <Text>List view screen</Text>
+    <View style={GlobalStyles.container}>
+      <Text style={GlobalStyles.header}>List view screen</Text>
     </View>
   );
 }
