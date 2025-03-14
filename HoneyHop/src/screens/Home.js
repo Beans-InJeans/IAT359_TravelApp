5bed95b2061ca47129257e1d1a9968fab4cf9b78
Jenna Han <jennahan03@gmail.com>
2025-03-13 17:11:40 -0700

    Add button styling to Home.js

--- HoneyHop/src/screens/Home.js
+++ HoneyHop/src/screens/Home.js
@@ -2,29 +2,30 @@ import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';  
+import GlobalStyles from '../../styles/GlobalStyles';
 
 export default function Home() {
   const navigation = useNavigation(); 
 
   return (
-    <View style={styles.container}>
+    <View style={GlobalStyles.container}>
       <Text>Welcome to HoneyHop</Text>
       <StatusBar style="auto" />
-      <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-        <Text>Login</Text>
+      <TouchableOpacity 
+        style={GlobalStyles.button}
+        onPress={() => navigation.navigate('Login')}
+      >
+        <Text style={GlobalStyles.buttonText}>Login</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-        <Text>Sign Up</Text>
+      <TouchableOpacity 
+        onPress={() => navigation.navigate('SignUp')}
+        style={GlobalStyles.button}  
+      >
+        <Text style={GlobalStyles.buttonText}>Sign Up</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
