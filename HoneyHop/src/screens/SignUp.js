df2fc3e0bc792dbc44af70baa5fd7c2d295d48fd
Jenna Han <jennahan03@gmail.com>
2025-03-14 14:43:15 -0700

    Import dependencies for auth

--- HoneyHop/src/screens/SignUp.js
+++ HoneyHop/src/screens/SignUp.js
@@ -1,32 +1,34 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { TextInput } from 'react-native-web';
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { firebase_auth } from "../firebaseConfig";
 
 export default function SignUp() {
     return(
         <View>
             <Text>Sign Up</Text>
             <TextInput
-                onChangeText={onChangeText}
+                // onChangeText={onChangeText}
                 value={firstname}
                 placeholder='First Name'
             />
             <TextInput
-                onChangeText={onChangeText}
+                // onChangeText={onChangeText}
                 value={lastname}
                 placeholder='Last Name'
             />
             <TextInput
-                onChangeText={onChangeText}
+                // onChangeText={onChangeText}
                 value={email}
                 placeholder='Email'
             />
             <TextInput
-                onChangeText={onChangeText}
+                // onChangeText={onChangeText}
                 value={username}
                 placeholder='Username'
             />
             <TextInput
-                onChangeText={onChangeText}
+                // onChangeText={onChangeText}
                 value={password}
                 placeholder='Password'
             />
