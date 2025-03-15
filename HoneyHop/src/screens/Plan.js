fdc09f8f7b1df6a6241f44939b9a756e75b36199
Jenna Han <jennahan03@gmail.com>
2025-03-15 13:47:39 -0700

    Create Plan.js

---
+++ HoneyHop/src/screens/Plan.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Plan() {
+  return(
+    <View>
+      <Text>Add plans</Text>
+    </View>
+  );
+}
