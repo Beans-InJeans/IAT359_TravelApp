e9041e2f01d3c373a1da7c67688953edee4417ad
Jenna Han <jennahan03@gmail.com>
2025-03-24 14:35:41 -0700

    Add button for navigating to map
    Was previously deleted not sure why, just re-added it.

--- HoneyHop/src/screens/Timeline.js
+++ HoneyHop/src/screens/Timeline.js
@@ -114,6 +114,9 @@ export default function Timeline({ navigation }) {
             </View>
           </View>
         ))}
+        <TouchableOpacity onPress={() => navigation.navigate('Map')} style={styles.mapButton}>
+          <Text style={styles.mapButtonText}>View on Map</Text>
+        </TouchableOpacity>
       </ScrollView>
 
       <TouchableOpacity style={styles.fab} onPress={() => navigation.navigate('Plan')}>
