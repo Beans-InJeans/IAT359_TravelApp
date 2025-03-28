8075eabeb69980f0cc2934dcb8dba900363023f7
Jenna Han <jennahan03@gmail.com>
2025-03-27 17:02:00 -0700

    Add plan pins to map
    Functions and useStates are added but it doesnt actually work (they are currently commented out)

--- HoneyHop/src/screens/Map.js
+++ HoneyHop/src/screens/Map.js
@@ -10,13 +10,20 @@ export default function MapScreen() {
   const [city, setCity] = useState(null);                   // City name
   const [airport, setAirport] = useState(null);             // To airport address
   const [accommodation, setAccommodation] = useState(null); // Accommodation address
+  const [planNames, setPlanNames] = useState([]);           // Plan name
+  const [planLocations, setPlanLocations] = useState([]);   // Plan address
 
   // Coordinates
   const [airportCoordinates, setAirportCoordinates] = useState(null);
   const [accommodationCoordinates, setAccommodationCoordinates] = useState(null);
+  const [planCoordinates, setPlanCoordinates] = useState([]);
   const [region, setRegion] = useState(null);
 
-  // Loading state
+  // Loading states
+  const [isCityLoading, setIsCityLoading] = useState(true);
+  const [isAirportLoading, setIsAirportLoading] = useState(true);
+  const [isAccommodationLoading, setIsAccommodationLoading] = useState(true);
+  const [isPlansLoading, setIsPlansLoading] = useState(true);
   const [loading, setLoading] = useState(true);
 
   // Data from FireStore
@@ -26,13 +33,13 @@ export default function MapScreen() {
   // On first render, fetch flight, accommodation, and plan details
   useEffect(() => {
     fetchTripData();  // Flights, accommodation
-    fetchPlans();     // Food, activity
+    // fetchPlans();     // Food, activity
   }, []);
 
   // Get city coordinates and set map region
   useEffect(() => {
     const fetchCoordinates = async () => {
-      if (tripData?.tripName) {
+      if (tripData?.tripName && !region) {
         // Get the coordinates from city name
         const coordinates = await fetchCityCoordinates(tripData.tripName);
         if (coordinates) {
@@ -43,26 +50,32 @@ export default function MapScreen() {
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           });
-          setLoading(false);  // Set loading to false once the region is set
+          setIsCityLoading(false);  // Set loading to false once the region is set
+          checkLoadingStates();
         } else {
           console.log("City coordinates not found.");
         }
       }
     };
 
-    fetchCoordinates();
+    // Prevent unnecessary calls if region already set
+    if (!region && tripData?.tripName) {
+      fetchCoordinates();
+    }
   }, [tripData]);
 
 
   // Get and set airport coordinates
   useEffect(() => {
-    if (airport) {
+    // Only fetch if airport coordinates aren't already set
+    if (airport && !airportCoordinates) {
       const fetchAndSetAirportCoords = async () => {
         const coordinates = await fetchAirportCoordinates();
         if (coordinates) {
           setAirportCoordinates(coordinates);
           console.log("Airport coordinates set.");
-          setLoading(false);
+          setIsAirportLoading(false);
+          checkLoadingStates();
         } else {
           console.log("No coordinates found for the airport.");
         }
@@ -74,13 +87,15 @@ export default function MapScreen() {
 
   // Get and set accommodation coordinates
   useEffect(() => {
-    if (accommodation) {
+    // Only fetch if accommodation coordinates aren't already set
+    if (accommodation && !accommodationCoordinates) {
       const fetchAndSetAccommodationCoords = async () => {
         const coordinates = await fetchAccommodationCoordinates();
         if (coordinates) {
           setAccommodationCoordinates(coordinates);
           console.log("Accommodation coordinates set.");
-          setLoading(false);
+          setIsAccommodationLoading(false);
+          checkLoadingStates();
         } else {
           console.log("No coordinates found for accommodation.");
         }
@@ -90,9 +105,42 @@ export default function MapScreen() {
     }
   }, [accommodation]);
 
+  // // Get and set plan coordinates
+  // useEffect(() => {
+  //   if (planLocations.length > 0 && planCoordinates.length === 0) {
+  //     const fetchAndSetPlanCoords = async () => {
+  //       const coordinates = await fetchPlanCoordinates();
+  //       if (coordinates.length > 0) {
+  //         setPlanCoordinates(coordinates);
+  //         console.log("Plan coordinates set: ", {planCoordinates});
+  //         setIsPlansLoading(false);
+  //         checkLoadingStates();
+  //       } else {
+  //         console.log("No coordinates found for plans.");
+  //       }
+  //     };
+
+  //     fetchAndSetPlanCoords();
+  //   }
+  // }, [planLocations]);
+
+  // A simple function that enforces a delay
+  const delay = ms => new Promise(resolve => {
+    console.log(`Delaying for ${ms}ms`);
+    setTimeout(resolve, ms);
+  });
+
+  // Set loading to false after all data is fetched
+  const checkLoadingStates = () => {
+    if (!isCityLoading && !isAirportLoading && !isAccommodationLoading && !isPlansLoading) {
+      setLoading(false);
+    }
+  };
+
   // Fetch coordinates from city name
   async function fetchCityCoordinates() {
     try {
+      await delay(1000);
       const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(city)}`);
       const data = await response.json();
 
@@ -110,7 +158,8 @@ export default function MapScreen() {
   // Fetch airport coordinates from airport address or name
   async function fetchAirportCoordinates() {
     try {
-      if (airport) {
+      if (airport && !airportCoordinates) {
+        await delay(1000);
         const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(airport)}`);
         const data = await response.json();
 
@@ -131,7 +180,8 @@ export default function MapScreen() {
   // Fetch accommodation coordinates from address or name
   async function fetchAccommodationCoordinates() {
     try {
-      if (airport) {
+      if (accommodation && !accommodationCoordinates) {
+        await delay(1000);
         const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(accommodation)}`);
         const data = await response.json();
 
@@ -148,6 +198,36 @@ export default function MapScreen() {
       console.error("Error fetching accommodation coordinates: ", error);
     }
   }
+
+  // // Fetch plan coordinates from address or name
+  // async function fetchPlanCoordinates() {
+  //   let coordinates = [];
+  //   for (const loc of planLocations) {
+  //     try {
+  //       if (loc) {
+  //         // Wait for the delay before making the fetch call
+  //         await delay(1000);
+  //         const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(loc)}`);
+  //         const data = await response.json();
+
+  //         if (data.length > 0) {
+  //           const coords = {
+  //             latitude: parseFloat(data[0].lat),
+  //             longitude: parseFloat(data[0].lon),
+  //           };
+  //           coordinates.push(coords);
+  //         }
+  //       } else {
+  //         console.log("Location is empty.");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching plan coordinates: ", error);
+  //     }
+  //   }
+
+  //   console.log("Plan coordinates returned: ", {coordinates});
+  //   return coordinates;
+  // }
 
   // Fetch all trip data (City, accommodation, flights) from FireStore
   async function fetchTripData() {
@@ -176,18 +256,32 @@ export default function MapScreen() {
     }
   }
 
-  // Fetch all plans (food, acitivities) from FireStore
-  function fetchPlans() {
-    const user = firebase_auth.currentUser;
-    if (!user) return;
+  // // Fetch all plans (food, acitivities) from FireStore
+  // function fetchPlans() {
+  //   const user = firebase_auth.currentUser;
+  //   if (!user) return;
 
-    const plansCollectionRef = collection(doc(db, "users", user.uid), "plans");
-    onSnapshot(plansCollectionRef, (snapshot) => {
-      const newPlans = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-      setPlans(newPlans);
-      console.log("Plans set: ", plans);
-    });
-  }
+  //   const plansCollectionRef = collection(doc(db, "users", user.uid), "plans");
+  //   onSnapshot(plansCollectionRef, (snapshot) => {
+  //     const newPlans = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+  //     // Get the object array from FireStore and put it into plans
+  //     console.log("Plans fetched: ", newPlans);
+  //     setPlans(newPlans);
+  //     console.log("Plans set: ", plans);
+
+  //     // Get the plan name from each plan in the array and put it in planNames
+  //     const names = newPlans.map((plan) => plan.activityTitle); 
+  //     setPlanNames(names);
+
+  //     // Get the plan location from each plan in the array and put it in planLocations
+  //     const locations = newPlans.map((plan) => plan.location); 
+  //     setPlanLocations(locations);
+      
+  //     // Check names and locations
+  //     console.log("Plan names and locations set: ", { names, locations });
+  //   });
+  // }
 
   if (loading || !region) {
     return (
@@ -234,6 +328,19 @@ export default function MapScreen() {
           title={accommodation} // Set the title for the marker (airport name)
         />
       )}
+
+      {/* {planCoordinates && planCoordinates.length > 0 && planNames && planNames.length > 0 && (
+        planCoordinates.map((coords, index) => (
+          <Marker
+            key={index} // Ensure unique key for each Marker
+            coordinate={{
+              latitude: coords.latitude,
+              longitude: coords.longitude,
+            }}
+            title={planNames[index]} // Use the title from the planNames array
+          />
+        ))
+      )} */}
       </MapView>
     </View>
   );
