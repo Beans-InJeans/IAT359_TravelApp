6dc773f8e00c5c07027d1f950009438f556451cb
clarissefaye <clarissefayetan2004@gmail.com>
2025-03-31 20:52:53 -0700

    Clean up and comment AppNavigator.js

--- HoneyHop/navigation/AppNavigator.js
+++ HoneyHop/navigation/AppNavigator.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { View, Image } from "react-native";
 import Home from "../src/screens/Home";
@@ -10,11 +9,11 @@ import Timeline from "../src/screens/Timeline";
 import Map from "../src/screens/Map";
 import List from "../src/screens/List";
 import Plan from "../src/screens/Plan";
-import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+//Bottom tab navigation for timeline and map
 function TimelineMapTabs() {
     return (
         <Tab.Navigator screenOptions={{ headerShown: false }}>
@@ -24,13 +23,14 @@ function TimelineMapTabs() {
     );
 }
 
+//Stack navigation for the app
 function AppNavigator() {
     return (
         <Stack.Navigator
             initialRouteName="Home"
             screenOptions={({ route }) => ({
-                headerTintColor: "#000", // Gold text/icons
-                headerTitleAlign: "right", // Align the logo to the right
+                headerTintColor: "#000", 
+                headerTitleAlign: "right", 
                 headerRight: () => (
                     <View>
                         <Image
@@ -39,18 +39,19 @@ function AppNavigator() {
                         />
                     </View>
                 ),
-                // Conditionally set headerStyle based on screen
+                //Conditionally set headerStyle based on screen
                 headerStyle: {
                     backgroundColor: 
                         route.name === "Home" || route.name === "Login" || route.name === "SignUp" 
-                        ? "#ffe850" // Yellow color
-                        : "#FAFAFA", // Light gray color for other screens
+                        ? "#ffe850" 
+                        : "#FAFAFA", 
                     elevation: 0,
                     shadowOpacity: 0,
                 },
-                headerTitle: "", // Hide the title
+                headerTitle: "", 
             })}
         >
+            {/* Define stack screens */}
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="SignUp" component={SignUp} />
